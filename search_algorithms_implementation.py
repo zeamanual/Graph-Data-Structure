3cda88f207bada7e1cbd842b507e289eaa7ae86f
abe <zeamanualfeleke@gmail.com>
2022-04-10 16:56:10 +0300

    a * search algorithm  implemented along with a function to calculate the huristic values

--- search_algorithms_implementation.py
+++ search_algorithms_implementation.py
@@ -99,3 +99,98 @@ def dijkstra(graph,start_node,end_node):
         previous=result[previous]['via']
     path.reverse()
     return path
+
+def get_huristic_value(location_one,location_two):
+    RADIUS=6373.0
+    latitude_one = math.radians(location_one[0])
+    longitude_one = math.radians(location_one[1])
+    latitude_two = math.radians(location_two[0])
+    longitude_two = math.radians(location_two[1])
+
+    diffrence_in_longitude = longitude_two-longitude_one
+    diffrence_in_latitude = latitude_two -latitude_one
+
+    a = math.pow((math.sin(diffrence_in_latitude/2)),2) + math.cos(latitude_one)*math.cos(latitude_two)*math.pow((math.sin(diffrence_in_longitude/2)),2)
+    c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+
+    distance_between = RADIUS * c
+    return distance_between
+
+
+def a_star_search(graph,start,end,city_cordinates):
+    infinity=float('inf')
+    nodes=dfs_generic(graph,start)
+    search_data ={}
+    
+    if(end not in nodes):
+        return []
+    for node in nodes:
+        search_data[node]={'F':infinity,'G':infinity,'from':None}
+
+    search_data[start]['G']=0
+    search_data[start]['F']=get_huristic_value(city_cordinates[start.node_identifier],city_cordinates[end.node_identifier])
+    to_be_visited=[]
+    visited=[]
+    to_be_visited.append(start)
+    while(len(to_be_visited)>0):
+        smallest_f = infinity
+        current_node = None
+        for node in search_data:
+            if(search_data[node]['F'] <= smallest_f and node  in to_be_visited):
+                smallest_f=search_data[node]['F']
+                current_node=node
+        if(current_node==end):
+            previous = search_data[current_node]['from']
+            reconstructed_path=[]
+            reconstructed_path.append(current_node)
+            while (previous!=None):
+                reconstructed_path.insert(0,previous)
+                previous=search_data[previous]['from']
+            return reconstructed_path
+                
+        for neighbour in graph.graph[current_node]['neighbours']:
+            if (neighbour in visited):
+                continue
+            else:
+                if( neighbour not in to_be_visited ):
+                    to_be_visited.append(neighbour)
+                if( (search_data[current_node]['G']+graph.graph[current_node]['neighbours'][neighbour]) < search_data[neighbour]['G'] ):
+                    search_data[neighbour]['G']=search_data[current_node]['G']+graph.graph[current_node]['neighbours'][neighbour]
+                    huristic_value = get_huristic_value(city_cordinates[neighbour.node_identifier],city_cordinates[end.node_identifier])
+                    search_data[neighbour]['F']=search_data[neighbour]['G']+huristic_value
+                    search_data[neighbour]['from']=current_node
+        to_be_visited.remove(current_node)
+        visited.append(current_node)
+    return []
+
+# add randome nodes to graph with specified size,
+# geneate random edges between them,
+# genearate random cordinates for the huristic value
+def add_random_node_to_graph(graph,size,existing_city_coordinates):
+
+    #generate random nodes
+    new_nodes=[]
+    for i in range(size):
+        letters = string.ascii_lowercase
+        random_word = ''.join(random.choice(letters) for i in range(7))
+        node = Node(random_word)
+        new_nodes.append(node)
+
+    #create random edge
+    for i in range(len(new_nodes)):
+        node1 = new_nodes[random.randint(0,len(new_nodes)-1)]
+        if(len(graph.graph.keys())>10):
+            node2 = list(graph.graph.keys())[random.randint(0,(len(new_nodes) % len(graph.graph.keys())))]
+        else:
+            node2 = new_nodes[random.randint(0,len(new_nodes)-1)]
+        edge = Edge(node1,node2)
+        graph.add_edge(edge)
+
+    #create random coordinates
+    for i in new_nodes:
+        latitude=(random.random() * 90)
+        longitude=(random.random()*180)
+        existing_city_coordinates[i.node_identifier]=[latitude,longitude]
+    
+    return graph,city_cordinates
+
